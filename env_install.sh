a0a64472f5e3ed3d7675dd33c3974556659f0faa
chensem <chensem@live.cn>
2018-09-25 11:43:18 +0800

    commit file

--- env_install.sh
+++ env_install.sh
@@ -7,7 +7,7 @@ cd ~
 
 sudo apt update
 sudo apt -y install zsh
-sudo apt -y install python-pip git ruby vim curl
+sudo apt -y install python-pip git ruby vim curl ipython
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -33,15 +33,20 @@ sudo pip2 install pwntools
 # install LibcSearcher
 git clone https://github.com/lieanu/libc.git
 cd libc
+sudo python setup.py develop
+
 ## change the original database
 rm -rf libc_database
 git clone https://github.com/niklasb/libc-database
+
 cd libc_database 
 chmod 755 get
-./get
+./get &
 
 sudo python setup.py develop
 
+# into home dir
+cd ~
 
 # install one_gadget
-gem install one_gadget
+sudo gem install one_gadget
